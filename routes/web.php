53f8ecc1d74efcffb730dca85976edbe1e51239a
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2025-11-08 23:07:01 +0700

    Tambah Routing untuk add member dan store data

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,21 @@
 
 use Illuminate\Support\Facades\Route; // BENAR
 
+
+
 Route::get('/home', 'PageController@home')->name('home');
 
-Route::get('/member', 'PageController@member')->name('member');
+
 
 Route::get("/class", "PageController@class")->name("class");
+
+//Routing untuk get member
+Route::get('/member', 'PageController@member')->name('member');
+// Routing ke form tambah member
+Route::get('/member/add-member', "PageController@addMemberForm")->name('add-member');
+//Routing untuk save data form
+Route::post('/member/add-member/save', 'PageController@saveMember')->name('save-member');
+
 
 // MemberGym
 
