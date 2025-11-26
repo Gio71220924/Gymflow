f292ec2de732c2e6df3e4afd4dce50e552725df9
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2025-11-26 10:15:55 +0700

    Feat: Tambahkan Middleware sesuai dengan hak akses

--- routes/web.php
+++ routes/web.php
@@ -5,40 +5,47 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthentikasiController;
 
-// Dashboard
-Route::get('/home', [PageController::class, 'home'])->name('home');
 
-// Classes (rename method agar tidak bentrok keyword)
-Route::get('/class', [PageController::class, 'classPage'])->name('class');
+Route::group(['middleware' => ['auth']], function () {
 
-// Billing
-Route::get('/billing', [PageController::class, 'billing'])->name('billing');
-Route::put('/billing/{id}', [PageController::class, 'updateInvoiceStatus'])->name('billing.update');
-Route::get('/billing/{id}/print', [PageController::class, 'printInvoice'])->name('billing.print');
+    // Dashboard
+    Route::get('/home', [PageController::class, 'home'])->name('home');
 
-// Settings
-Route::get('/settings', [PageController::class, 'settings'])->name('settings');
-Route::post('/settings', [PageController::class, 'saveSettings'])->name('settings.save');
+    // Classes (rename method agar tidak bentrok keyword)
+    Route::get('/class', [PageController::class, 'classPage'])->name('class');
 
-// Member
-Route::get('/member', [PageController::class, 'member'])->name('member');
-Route::get('/member/add-member', [PageController::class, 'addMemberForm'])->name('add-member');
-Route::post('/member/add-member/save', [PageController::class, 'saveMember'])->name('save-member');
-Route::get('/member/edit-member/{id}', [PageController::class, 'editMemberForm'])->name('edit-member');
-Route::put('/member/edit-member/update/{id}', [PageController::class, 'updateMember'])->name('update-member');
+    // Billing
+    Route::get('/billing', [PageController::class, 'billing'])->name('billing');
+    Route::put('/billing/{id}', [PageController::class, 'updateInvoiceStatus'])->name('billing.update');
+    Route::get('/billing/{id}/print', [PageController::class, 'printInvoice'])->name('billing.print');
 
-// Hapus: idealnya DELETE, tapi kalau masih pakai link GET boleh aktifkan salah satu:
-Route::delete('/member/delete-member/{id}', [PageController::class, 'deleteMember'])->name('delete-member');
-// Sementara fallback GET (kalau tombol masih <a href="...">):
-Route::get('/member/delete-member/{id}', [PageController::class, 'deleteMember']);
+    // Settings
+    Route::get('/settings', [PageController::class, 'settings'])->name('settings');
+    Route::post('/settings', [PageController::class, 'saveSettings'])->name('settings.save');
 
-// Users (CRUD by admin)
-Route::get('/users', [UserController::class, 'getuser'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    // Member
+    Route::get('/member', [PageController::class, 'member'])->name('member');
+    Route::get('/member/add-member', [PageController::class, 'addMemberForm'])->name('add-member');
+    Route::post('/member/add-member/save', [PageController::class, 'saveMember'])->name('save-member');
+    Route::get('/member/edit-member/{id}', [PageController::class, 'editMemberForm'])->name('edit-member');
+    Route::put('/member/edit-member/update/{id}', [PageController::class, 'updateMember'])->name('update-member');
 
-// Auth
-Route::get('/', [AuthentikasiController::class, 'loginForm'])->name('login');
-Route::post('/CekLogin' , [AuthentikasiController::class, 'cekLogin'])->name('cekLogin');
-Route::post('/logout', [AuthentikasiController::class, 'logout'])->name('logout');
+    // Hapus: idealnya DELETE, tapi kalau masih pakai link GET boleh aktifkan salah satu:
+    Route::delete('/member/delete-member/{id}', [PageController::class, 'deleteMember'])->name('delete-member');
+    // Sementara fallback GET (kalau tombol masih <a href="...">):
+    Route::get('/member/delete-member/{id}', [PageController::class, 'deleteMember']);
+
+    // Users (CRUD by admin)
+    Route::get('/users', [UserController::class, 'getuser'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    
+    // Logout   
+    Route::post('/logout', [AuthentikasiController::class, 'logout'])->name('logout');
+    });
+
+Route::group(['middleware' => ['guest']], function () {
+    Route::get('/', [AuthentikasiController::class, 'loginForm'])->name('login');
+    Route::post('/CekLogin' , [AuthentikasiController::class, 'cekLogin'])->name('cekLogin');
+});
